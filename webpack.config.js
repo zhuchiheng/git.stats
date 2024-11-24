7579c5d218771537867e9d33707861c49cdbd809
xmli <lixianmin@live.cn>
2024-11-24 10:16:58 +0800

    打包上传到 https://marketplace.visualstudio.com/manage/publishers/lixianmin

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+//@ts-check
+'use strict';
+
+const path = require('path');
+
+/** @type {import('webpack').Configuration} */
+const config = {
+  target: 'node',
+  mode: 'none',
+  entry: './src/extension.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2'
+  },
+  externals: {
+    vscode: 'commonjs vscode',
+    // 不打包这些依赖，因为它们在运行时会被正确加载
+    'simple-git': 'commonjs simple-git',
+    'moment': 'commonjs moment'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  },
+  devtool: 'nosources-source-map',
+  infrastructureLogging: {
+    level: "log",
+  },
+};
+
+module.exports = config;
